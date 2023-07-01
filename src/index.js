a37b0e047ef29830d096d1846037eaa4c5e29634
tarastupko <109355475+tarastupko@users.noreply.github.com>
2023-07-01 14:44:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import './css/style.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
@@ -9,8 +8,9 @@ import getFetch from './js/api-service'
 
 // variables
 
-const axios = require('axios').default;
+// variables
 
+const axios = require('axios').default;
 
 const forma = document.querySelector('.search-form');
 const list = document.querySelector('.search-list-js')
@@ -26,92 +26,77 @@ let options = {
   threshold: 1.0
 };
 
-let observer = new IntersectionObserver(onLoad, options);
+let observer = null; // Початково не створюємо IntersectionObserver
 let currentPage = 1
-
-
+let currentPageOnLoad = 1;
 
 forma.addEventListener('submit', onClickSubmit);
 
-
 function onClickSubmit(event) {
-
   event.preventDefault();
-  currentPage = 1
-  getImages(1, 40)
+  currentPage = 1;
+  currentPageOnLoad = 1; // Скидаємо значення currentPageOnLoad при кожному новому пошуку
+  getImages(1, 40);
 }
-
-
 
 async function getImages(page, perPage) {
-
   if (inputSer.value.trim() === '') {
     return Notiflix.Notify.failure(`Request cannot be empty`);
-}
+  }
 
   try {
-    const searchResponse = inputSer.value.trim()
-    const response = await getFetch(page, perPage,searchResponse);
-    
-        if (response.data.hits.length === 0) {
-          Notiflix.Notify.failure(`Oooops! No images found for query <i>'${inputSer.value}</i>'`);
-          list.innerHTML = `<p>Oooops! No images found for query <i>'${inputSer.value}</i>'. Try again!</p>`
-          return
-        }
-        
-        Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images`);
-        
-        console.log(response.data);
-        
-    list.innerHTML = createMarkup(response.data.hits)
-    console.log(response.data.totalHits);
-    if (response.data.totalHits > perPage)
-    {
-      observer.observe(target);
-  }
-      
-        
-        
-            
-        gallerySlider = new SimpleLightbox('.gallery a',
-          { captionsData: 'alt', captionDelay: 950, navText: ['❮', '❯'] });
+    const response = await getFetch(page, perPage, inputSer.value.trim());
 
-      } catch(erorr) {
-            console.error(erorr)
-        }
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure(`Oooops! No images found for query <i>'${inputSer.value}</i>'`);
+      list.innerHTML = `<p>Oooops! No images found for query <i>'${inputSer.value}</i>'. Try again!</p>`
+      return;
+    }
+
+    Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images`);
+
+    list.innerHTML = createMarkup(response.data.hits);
+
+    if (response.data.totalHits > perPage) {
+      // Створюємо IntersectionObserver тільки після другого пошуку
+      if (observer === null) {
+        observer = new IntersectionObserver(onLoad, options);
+      }
+      observer.observe(target);
+    }
+
+    gallerySlider = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 950,
+      navText: ['❮', '❯']
+    });
+
+    currentPageOnLoad = page; // Оновлення значення currentPageOnLoad
+
+  } catch (error) {
+    console.error(error);
+  }
 }
-let currentPageOnLoad = 2;
+
 function onLoad(entries, observer) {
-  
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      console.log(entries);
-      const searchResponse = inputSer.value.trim()
-      getFetch(currentPageOnLoad, perPage,searchResponse)
-          .then(response => {
-          console.log();
-              list.insertAdjacentHTML('beforeend', createMarkup(response.data.hits))
-            gallerySlider.refresh()
-              if (currentPageOnLoad * perPage >= response.data.totalHits) {
-              setTimeout(() => {
-                Notiflix.Notify.info(`We're sorry, but you've reached the end of search results`);
-              }, 2000);
-                observer.unobserve(target)
-                return
-              }
-              else
-              {
-                currentPageOnLoad += 1
-            observer.observe(target); 
-                }
-             
-
-          
-            
-            smoothScroll()
-            
-          })
-  .catch(err => console.log(err));
-  }
-  })
-  }
+      currentPageOnLoad += 1;
+      getFetch(currentPageOnLoad, perPage, inputSer.value.trim())
+        .then(response => {
+          list.insertAdjacentHTML('beforeend', createMarkup(response.data.hits));
+          gallerySlider.refresh();
+          if (currentPageOnLoad * perPage >= response.data.totalHits) {
+            setTimeout(() => {
+              Notiflix.Notify.info(`We're sorry, but you've reached the end of search results`);
+            }, 2000);
+            observer.unobserve(target);
+            return;
+          }
+          observer.observe(target);
+          smoothScroll();
+        })
+        .catch(err => console.log(err));
+    }
+  });
+}
